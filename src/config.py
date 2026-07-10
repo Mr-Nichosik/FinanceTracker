e8638abaa36a97e865cfb4c89fd16f4a6c9590ad
Mr Nichosik <asyamovilya@gmail.com>
2026-07-10 12:50:50 +0700

    add new menu items system

--- src/config.py
+++ src/config.py
@@ -6,3 +6,16 @@ meta = metadata("FinanceTracker")
 APP_NAME = meta["Name"]
 APP_VERSION = meta["Version"]
 APP_AUTHOR = meta["Author"]
+
+ERRORS = {
+    0: "<UnexpectedError>",
+    1: "<CommandNotFoundError>",
+    2: "<InvalidInputFormat>",
+    3: "<ValueMustBeANumber>",
+    4: "<ObjectNotFoundError>",
+}
+
+MESSAGES = {
+    0: "Сохранено", 
+    1: "Готово"
+}
--- src/console.py
+++ src/console.py
@@ -1,85 +1,126 @@
 
 import os
 from datetime import date
+from dataclasses import dataclass
+from typing import Callable
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.formatted_text import ANSI
 
 from category import Category
-from console_format import *
 from operation import Operation, OperationData, OperationType
 from finance_manager import FinanceManager, Balance, SortType
-from config import APP_NAME, APP_VERSION
+from config import APP_NAME, APP_VERSION, ERRORS, MESSAGES
+
+@dataclass(frozen=True)
+class Format:   
+    RESET = "\033[0m"
+
+    DEFAULT = "\033[0m"
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+    UNDERLINE = "\033[4m"
+    INVERTED = "\033[7m"
+
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+
+@dataclass(frozen=True)
+class MenuItem:
+    key: str = ""
+    description: str = ""
+    action:  Callable[[], None] | str = ""
+    prefix: str | None = None
 
 class Console:
     def __init__(self):
+        # self.__main_commands = [
+        #     {
+        #         "id": "-1",
+        #         "desc": "[DEBUG] Shows task by id",
+        #         "prefix": "DEBUG",
+        #         "action": self.__show_operation_by_id,
+        #     },
+        #     {
+        #         "id": "0",
+        #         "desc": "[DEBUG] Shows all tasks",
+        #         "prefix": "DEBUG",
+        #         "action": self.__show_all_operations,
+        #     },
+        #     {
+        #         "id": "v",
+        #         "desc": "Показать версию",
+        #         "prefix": None,
+        #         "action": self.__show_app_info,
+        #     },
+        #     {
+        #         "id": "1",
+        #         "desc": "Добавить операцию",
+        #         "prefix": None,
+        #         "action": self.add_operation,
+        #     },
+        #     {
+        #         "id": "2",
+        #         "desc": "Редактировать операцию",
+        #         "prefix": None,
+        #         "action": self.edit_operation,
+        #     },
+        #     {
+        #         "id": "3",
+        #         "desc": "Удалить операцию",
+        #         "prefix": None,
+        #         "action": self.remove_operation,
+        #     },
+        #     {
+        #         "id": "4",
+        #         "desc": "Фильтр",
+        #         "prefix": None,
+        #         "action": self.show_filtered_operations,
+        #     },
+        #     {
+        #         "id": "5",
+        #         "desc": "Управление категориями",
+        #         "prefix": None,
+        #         "action": self.categories_control,
+        #     },
+        #     {
+        #         "id": "6",
+        #         "desc": "Показать баланс",
+        #         "prefix": None,
+        #         "action": self.show_balance,
+        #     },
+        #     {
+        #         "id": "7",
+        #         "desc": "Показать статистику по категориям",
+        #         "prefix": None,
+        #         "action": self.show_statistics_by_categories,
+        #     },
+        #     {
+        #         "id": "8",
+        #         "desc": "Показать последние N операций",
+        #         "prefix": None,
+        #         "action": self.show_last_n_operations,
+        #     },
+        # ]
+
         self.__main_commands = [
-            {
-                "id": "-1",
-                "desc": "[DEBUG] Shows task by id",
-                "prefix": "DEBUG",
-                "action": self.__show_operation_by_id,
-            },
-            {
-                "id": "0",
-                "desc": "[DEBUG] Shows all tasks",
-                "prefix": "DEBUG",
-                "action": self.__show_all_operations,
-            },
-            {
-                "id": "v",
-                "desc": "Показать версию",
-                "prefix": None,
-                "action": self.__show_app_info,
-            },
-            {
-                "id": "1",
-                "desc": "Добавить операцию",
-                "prefix": None,
-                "action": self.add_operation,
-            },
-            {
-                "id": "2",
-                "desc": "Редактировать операцию",
-                "prefix": None,
-                "action": self.edit_operation,
-            },
-            {
-                "id": "3",
-                "desc": "Удалить операцию",
-                "prefix": None,
-                "action": self.remove_operation,
-            },
-            {
-                "id": "4",
-                "desc": "Фильтр",
-                "prefix": None,
-                "action": self.show_filtered_operations,
-            },
-            {
-                "id": "5",
-                "desc": "Управление категориями",
-                "prefix": None,
-                "action": self.categories_control,
-            },
-            {
-                "id": "6",
-                "desc": "Показать баланс",
-                "prefix": None,
-                "action": self.show_balance,
-            },
-            {
-                "id": "7",
-                "desc": "Показать статистику по категориям",
-                "prefix": None,
-                "action": self.show_statistics_by_categories,
-            },
-            {
-                "id": "8",
-                "desc": "Показать последние N операций",
-                "prefix": None,
-                "action": self.show_last_n_operations,
-            },
+            MenuItem("-1", "[DEBUG] Show operation by id", self.__show_operation_by_id, "DEBUG"),
+            MenuItem("0", "[DEBUG] Show all operations", self.__show_all_operations, "DEBUG"),
+            MenuItem("v", "Показать версию", self.__show_app_info),
+            MenuItem("1", "Добавить операцию", self.add_operation),
+            MenuItem("2", "Редактировать операцию", self.edit_operation),
+            MenuItem("3", "Удалить операцию", self.remove_operation),
+            MenuItem("4", "Фильтр", self.show_filtered_operations),
+            MenuItem("5", "Управление категориями", self.categories_control),
+            MenuItem("6", "Показать баланс", self.show_balance),
+            MenuItem("7", "Показать статистику по категориям", self.show_statistics_by_categories),
+            MenuItem("8", "Показать последние N операций", self.show_last_n_operations),
         ]
 
         self.__category_commands = [
@@ -102,65 +143,55 @@ class Console:
             },
         ]
 
-        self.__sort_types = [
+        self.__sort_type_commands = [
             {"id": 1, "desc": "По дате - 1", "action": SortType.DATE},
             {"id": 2, "desc": "По сумме - 2", "action": SortType.AMOUNT},
             {"id": 3, "desc": "По категории - 3", "action": SortType.CATEGORY},
         ]
 
-        self.__errors = {
-                0: "<UnexpectedError>",
-                1: "<CommandNotFoundError>",
-                2: "<InvalidInputFormat>",
-                3: "<ValueMustBeANumber>",
-                4: "<ObjectNotFoundError>"
-            }
-        
-        self.__messages = {
-                0: "Сохранено",
-                1: "Готово"
-            }
-
         self.manager = FinanceManager()
 
-    def __prompt(self, prompt_text: str ="", default_text: str = "", multiline=False) -> str:
+    def __prompt(self, prompt_text: str="", default_text: str="", multiline=False) -> str:
         session = PromptSession(multiline=multiline)
-        return session.prompt(ANSI(f"{CYAN}{prompt_text}> {RESET}"), default=default_text, prompt_continuation=ANSI(f"{CYAN}>{RESET}"))
+        return session.prompt(ANSI(f"{Format.CYAN}{prompt_text}> {Format.RESET}"), default=default_text, prompt_continuation=ANSI(f"{Format.CYAN}>{Format.RESET}"))
 
     def __input_command(self):
         return self.__prompt()
 
-    def __run_menu(self, commands: list[dict], showWithPrefix=False, clearScreen=False):
+    def __run_menu(self, commands: list[MenuItem], showWithPrefix=False, clearScreen=False):
         print("Доступные действия:")
 
         for i in commands:
-            if showWithPrefix == True and i.get("prefix"):
-                print(f"{i.get("id")}. {i.get("desc")}")
+            if showWithPrefix == True and i.prefix:
+                print(f"{i.key}. {i.description}")
 
-            if not i.get("prefix"):
-                print(f"{i.get("id")}. {i.get("desc")}")
+            if not i.prefix:
+                print(f"{i.key}. {i.description}")
 
-        command_id = self.__input_command()
+        key_value = self.__input_command()
         if clearScreen:
             os.system("cls")
 
-        command = next(
-            (item for item in commands if item.get("id") == command_id), None
-        )
+        command = MenuItem()
+
+        for cmd in commands:
+            if cmd.key == key_value:
+                command = cmd
+
         if command != None:
-            if command.get("prefix"):
-                print(f"[{command.get("prefix")}]")
-            command.get("action")()
+            if command.prefix:
+                print(f"[{command.prefix}]")
+            command.action()
         else:
-            self.__show_message(self.__errors.get(1))
+            self.__show_message(ERRORS[1])
 
     def __show_message(self, text: str, positive=False, description=""):
         
-        color = GREEN if positive else RED
-        print(color, text, RESET)
+        color = Format.GREEN if positive else Format.RED
+        print(color, text, Format.RESET)
 
         if description != "":
-            print(BLUE, description, RESET)
+            print(Format.BLUE, description, Format.RESET)
 
     def __show_operation(self, op: Operation):
         category = self.manager.get_category(op.category_id)
@@ -183,7 +214,7 @@ class Console:
                 self.__show_operation(self.manager.get_operation(operation_id))
                 return
             except ValueError:
-                self.__show_message(self.__errors.get(3))
+                self.__show_message(ERRORS.get(3))
 
     def __show_category(self, cat: Category):
         print(f"id: {cat.id} | название: {cat.title}")
@@ -202,7 +233,7 @@ class Console:
                 id = int(self.__prompt())
                 return id
             except ValueError:
-                self.__show_message(self.__errors.get(3))
+                self.__show_message(ERRORS.get(3))
 
     def __get_operation_data(self, data: OperationData | None = None) -> OperationData:
         if data is None:
@@ -221,7 +252,7 @@ class Console:
                 data["type"] = FinanceManager.require(OperationType.get_type(value))
                 break
 
-            self.__show_message(self.__errors.get(2))
+            self.__show_message(ERRORS.get(2))
 
         print("Категория:")
         while True:
@@ -239,7 +270,7 @@ class Console:
                     data["category_id"] = self.manager.get_category_by_title(value).id
                     break
 
-            self.__show_message(self.__errors.get(2))
+            self.__show_message(ERRORS.get(2))
 
         print("Заголовок:")
         while True:          
@@ -248,7 +279,7 @@ class Console:
                 data["title"] = value
                 break
 
-            self.__show_message(self.__errors.get(2))
+            self.__show_message(ERRORS.get(2))
 
         print("Сумма (формат дробных чисел - 100.5)")
         while True:
@@ -258,9 +289,9 @@ class Console:
                     data["amount"] = float(value)
                     break
 
-                self.__show_message(self.__errors.get(2))
+                self.__show_message(ERRORS.get(2))
             except ValueError:
-                self.__show_message(self.__errors.get(3))
+                self.__show_message(ERRORS.get(3))
 
         print("Дата (Enter - текущая):")
         while True:
@@ -282,39 +313,39 @@ class Console:
             print(i.get("desc"))
 
     def __show_app_info(self):
-        print(f"{CYAN}{BOLD}{APP_NAME}, v{APP_VERSION}{RESET}")
+        print(f"{Format.CYAN}{Format.BOLD}{APP_NAME}, v{APP_VERSION}{Format.RESET}")
 
     def run(self):
         self.__show_app_info()
-        print(f"{BOLD}==============================={RESET}")
+        print(f"{Format.BOLD}==============================={Format.RESET}")
 
         while True:
             try:
                 self.__run_menu(self.__main_commands, False, True)
 
                 self.__prompt()
-                print(f"{RESET}")
+                print(f"{Format.RESET}")
                 os.system("cls")
 
             except EOFError:
-                print(f"{RESET}")
+                print(f"{Format.RESET}")
                 os.system("cls")
 
             except KeyboardInterrupt:
-                print(f"{BOLD}Работа завершена")
+                print(f"{Format.BOLD}Работа завершена")
                 raise SystemExit
 
     def add_operation(self):
         data: OperationData = self.__get_operation_data()
 
         self.__show_operation(self.manager.add_operation(data))
-        self.__show_message(self.__messages[0], True)
+        self.__show_message(MESSAGES[0], True)
 
     def edit_operation(self):
         id = self.__get_id()
 
         while self.manager.operation_exists(id) == False:
-            self.__show_message(self.__errors[4])
+            self.__show_message(ERRORS[4])
             id = self.__get_id()
 
         old_op: Operation = self.manager.get_operation(id)
@@ -324,17 +355,17 @@ class Console:
         new_op["id"] = id
 
         self.__show_operation(self.manager.edit_operation(old_op, new_op))
-        self.__show_message(self.__messages[0], True)
+        self.__show_message(MESSAGES[0], True)
 
     def remove_operation(self):
         id = self.__get_id()
 
         while self.manager.operation_exists(id) == False:
-            self.__show_message(self.__errors[4])
+            self.__show_message(ERRORS[4])
             id = self.__get_id()
 
         self.manager.remove_operation(id)
-        self.__show_message(self.__messages[1], True) 
+        self.__show_message(MESSAGES[1], True) 
 
     def show_filtered_operations(self):
         print("Нажмите Enter, чтобы пропустить условие")
@@ -350,14 +381,14 @@ class Console:
 
         print("Сортировать:")   
         while True:
-            self.__show_actions_list(self.__sort_types)
+            self.__show_actions_list(self.__sort_type_commands)
             sort_type_id = self.__prompt()
             
             if sort_type_id != "":
                 sort_type = FinanceManager.require(SortType.get_type(sort_type_id))
                 break
 
-            self.__show_message(self.__errors.get(2))
+            self.__show_message(ERRORS.get(2))
 
         info = [date, category, type_value]
         for i in range(len(info)):
@@ -381,13 +412,13 @@ class Console:
         cat = self.manager.add_category(title)
 
         self.__show_category(cat)
-        self.__show_message(self.__messages[0], True)
+        self.__show_message(MESSAGES[0], True)
 
     def edit_category(self):
         id = self.__get_id()
 
         while self.manager.category_exists(id) == False:
-            self.__show_message(self.__errors[4])
+            self.__show_message(ERRORS[4])
             id = self.__get_id()
 
         print("Укажите заголовок:")
@@ -396,13 +427,13 @@ class Console:
         cat = self.manager.edit_category(id, title)         
 
         self.__show_category(cat)
-        self.__show_message(self.__messages[0], True)
+        self.__show_message(MESSAGES[0], True)
 
     def remove_category(self):
         id = self.__get_id()
 
         while self.manager.category_exists(id) == False:
-            self.__show_message(self.__errors[4])
+            self.__show_message(ERRORS[4])
             id = self.__get_id()
 
         linked_operations = self.manager.category_linked(id)
@@ -411,7 +442,7 @@ class Console:
             return
 
         self.manager.remove_category(id)
-        self.__show_message(self.__messages[1], True)
+        self.__show_message(MESSAGES[1], True)
 
     def show_balance(self):
         data: Balance = self.manager.get_balance()
@@ -419,22 +450,22 @@ class Console:
         print(f"Доходы: {data["income"]}")
         print(f"Расходы: {data["expenses"]}")
 
-        print(f"{RED}Баланс: {data["balance"]}{RESET}") if data["balance"] < 0 else print(f"{GREEN}Баланс: {data["balance"]}{RESET}")
+        print(f"{Format.RED}Баланс: {data["balance"]}{Format.RESET}") if data["balance"] < 0 else print(f"{Format.GREEN}Баланс: {data["balance"]}{Format.RESET}")
 
     def show_statistics_by_categories(self):
         income = self.manager.get_amount_by_categories(OperationType.INCOME)
-        print(f"{BOLD}Доходы:{RESET}")
+        print(f"{Format.BOLD}Доходы:{Format.RESET}")
         for cat_id in income:
             self.__show_stats(cat_id, income.get(cat_id))
-        print(f"{BOLD}Итого: {self.manager.get_amount_by_op_type(OperationType.INCOME)}{RESET}")
+        print(f"{Format.BOLD}Итого: {self.manager.get_amount_by_op_type(OperationType.INCOME)}{Format.RESET}")
 
         print()
 
         expenses = self.manager.get_amount_by_categories(OperationType.EXPENSE)
-        print(f"{BOLD}Расходы:{RESET}")
+        print(f"{Format.BOLD}Расходы:{Format.RESET}")
         for cat_id in expenses:
             self.__show_stats(cat_id, expenses.get(cat_id))
-        print(f"{BOLD}Итого: {self.manager.get_amount_by_op_type(OperationType.EXPENSE)}{RESET}")
+        print(f"{Format.BOLD}Итого: {self.manager.get_amount_by_op_type(OperationType.EXPENSE)}{Format.RESET}")
 
     def show_last_n_operations(self):
         while True:
@@ -443,8 +474,9 @@ class Console:
                 n = int(self.__prompt())
                 if n > 0:
                     break
-                self.__show_message(self.__errors.get(3))
+                self.__show_message(ERRORS.get(3))
             except ValueError:
-                self.__show_message(self.__errors.get(3))
+                self.__show_message(ERRORS.get(3))
 
         self.__show_operations_list(self.manager.get_n_operations(n))
+
